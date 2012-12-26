ae7d04af8216d28760102ac6f273b7c9a7691500
Mike Petrovich <michael.c.petrovich@gmail.com>
2012-12-26 08:35:42 -0500

    Clip rewrite: Work in progress

--- ui/jquery.ui.effect-clip.js
+++ ui/jquery.ui.effect-clip.js
@@ -13,51 +13,80 @@
  */
 (function( $, undefined ) {
 
+var clipRegex = /^rect\((-?\d*\.?\d*px|-?\d+%|auto),?\s+(-?\d*\.?\d*px|-?\d+%|auto),?\s+(-?\d*\.?\d*px|-?\d+%|auto),?\s+(-?\d*\.?\d*px|-?\d+%|auto)\)$/,
+	parseClip = function( el ) {
+		var str = el.css("clip"),
+			outerWidth = el.outerWidth(),
+			outerHeight = el.outerHeight(),
+			values = clipRegex.exec( str ) || [ "", 0, outerWidth, outerHeight, 0 ];
+
+		return {
+			top: parseFloat( values[ 1 ] ) || 0 ,
+			right: parseFloat( values[ 2 ] ) || outerWidth,
+			bottom: parseFloat( values[ 3 ] ) || outerHeight,
+			left: parseFloat( values[ 4 ] ) || 0
+		};
+	};
+
 $.effects.effect.clip = function( o, done ) {
-	// Create element
-	var el = $( this ),
-		props = [ "position", "top", "bottom", "left", "right", "height", "width" ],
+	var start, end, placeholder, temp, position,
+		el = $( this ),
+		props = [ "display", "position", "left", "right", "width", "height", "clip" ],
 		mode = $.effects.setMode( el, o.mode || "hide" ),
 		show = mode === "show",
 		direction = o.direction || "vertical",
-		vert = direction === "vertical",
-		size = vert ? "height" : "width",
-		position = vert ? "top" : "left",
-		animation = {},
-		wrapper, animate, distance;
+		both = direction === "both",
+		horizontal = both || direction === "horizontal",
+		vertical = both || direction === "vertical";
 
-	// Save & Show
-	$.effects.save( el, props );
-	el.show();
-
-	// Create Wrapper
-	wrapper = $.effects.createWrapper( el ).css({
-		overflow: "hidden"
-	});
-	animate = ( el[0].tagName === "IMG" ) ? wrapper : el;
-	distance = animate[ size ]();
-
-	// Shift
 	if ( show ) {
-		animate.css( size, 0 );
-		animate.css( position, distance / 2 );
+		el.show();
 	}
 
-	// Create Animation Object:
-	animation[ size ] = show ? distance : 0;
-	animation[ position ] = show ? 0 : distance / 2;
+	position = el.position();
+	start = parseClip( el );
+	end = {
+		top: vertical ? ( start.bottom - start.top ) / 2 : start.top,
+		right: horizontal ? ( start.right - start.left ) / 2 : start.right,
+		bottom: vertical ? ( start.bottom - start.top ) / 2 : start.bottom,
+		left: horizontal ? ( start.right - start.left ) / 2 : start.left
+	};
 
-	// Animate
-	animate.animate( animation, {
+	$.effects.save( el, props );
+
+	placeholder = $.effects.createPlaceholder( el );
+
+	el.css({
+		position: placeholder ? "absolute" : el.css("position"),
+		left: position.left,
+		top: position.top
+	})
+	.outerWidth( el.outerWidth(true), true )
+	.outerHeight( el.outerHeight(true), true );
+
+	if ( show ) {
+		temp = start;
+		start = end;
+		end = temp;
+	}
+
+	$( start ).animate( end, {
 		queue: false,
 		duration: o.duration,
 		easing: o.easing,
+		step: function() {
+			el.css( "clip", "rect(" + this.top + "px " + this.right + "px " + this.bottom + "px " + this.left + "px)" );
+		},
 		complete: function() {
+			$.effects.restore( el, props );
+
+			if ( placeholder ) {
+				placeholder.remove();
+			}
 			if ( !show ) {
 				el.hide();
 			}
-			$.effects.restore( el, props );
-			$.effects.removeWrapper( el );
+
 			done();
 		}
 	});
